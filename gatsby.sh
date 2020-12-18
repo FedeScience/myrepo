0cc73335c39093fe58b8ec16904950ba7df083e4
Francesco Lorre <chiccofriendly@gmail.com>
2020-12-18 12:42:59 +0100

    blog

--- gatsby.sh
+++ gatsby.sh
@@ -7,3 +7,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 nvm install --lts
 npm install --global gatsby-cli
 gatsby new my-first-gatsby-site https://github.com/gatsbyjs/gatsby-starter-default
+gatsby new my-first-gatsby-blog https://github.com/gatsbyjs/gatsby-starter-blog
