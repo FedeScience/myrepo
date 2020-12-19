5a04f902e8fdc054863df6620b6864985c4375eb
Francesco Lorre <chiccofriendly@gmail.com>
2020-12-19 14:07:50 +0100

    HGA_Reinforced

---
+++ R_programmeR/Reporting.R
@@ -0,0 +1,13 @@
+#!/usr/bin/R
+#' ---
+#' title: "Crop Analysis Q3 2013"
+#' author: "John Smith"
+#' date: "May 3rd, 2014"
+#' output: pdf_document
+#' ---
+#' A script comment that includes **markdown** formatting.
+# knitr Spin
+#+ fig.width=5, fig.height=5
+
+# rmarkdown::render("analysis.R")
+# rmarkdown::render("analysis.R", "pdf_document")
