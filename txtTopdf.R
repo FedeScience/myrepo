94b87ef481f0f39762f59621f0a9051c3c41a329
Francesco Lorre <chiccofriendly@gmail.com>
2020-12-21 12:35:55 +0100

    Format Conversion

---
+++ txtTopdf.R
@@ -0,0 +1,5 @@
+require(rmarkdown)
+my_text <- readLines("~/Documenti/CoverLetter.txt") 
+cat(my_text, sep="  \n", file = "my_text.Rmd")
+render("my_text.Rmd", pdf_document())
+file.remove("my_text.Rmd") #cleanup
