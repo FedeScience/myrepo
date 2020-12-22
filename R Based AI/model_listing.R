3abb1684d7e41bbe335e27e2b13cc2d8f233013d
Francesco Lorre <chiccofriendly@gmail.com>
2020-12-22 10:52:55 +0100

    model listing

---
+++ R Based AI/model_listing.R
@@ -0,0 +1,5 @@
+#Model listing
+algoList = c('rpart','glm','knn','svmRadial')
+source(file = "R Based AI/Supervised/Classification/class-models-clustered-by-tag-similarity.R")
+source(file = "R Based AI/Supervised/Regression/reg-models-clustered-by-tag-similarity.R")
+source(file = "R Based AI/Supervised/MLmodelsCR.R")
